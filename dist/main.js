25221f5dabbdc446bcb321d8ceaadbab4eebd2fb
Ryan Tsuzaki <rtsuzaki10@gmail.com>
2019-01-31 17:10:44 -0800

    Add css for AddToBalance buttons

--- dist/main.js
+++ dist/main.js
@@ -237,7 +237,7 @@ eval("var g;\n\n// This works in non-strict mode\ng = (function() {\n\treturn th
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n\n\nvar AddToBalanceButton = function AddToBalanceButton(props) {\n  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(\"button\", {\n    type: \"button\",\n    onClick: function onClick() {\n      return props.addToBalance(props.addedVal);\n    }\n  }, \"Add $\", props.addedVal);\n};\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (AddToBalanceButton);\n\n//# sourceURL=webpack:///./src/AddToBalanceButton.jsx?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n\n\nvar AddToBalanceButton = function AddToBalanceButton(props) {\n  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(\"button\", {\n    type: \"button\",\n    className: \"add-button\",\n    onClick: function onClick() {\n      return props.addToBalance(props.addedVal);\n    }\n  }, \"Add $\", props.addedVal);\n};\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (AddToBalanceButton);\n\n//# sourceURL=webpack:///./src/AddToBalanceButton.jsx?");
 
 /***/ }),
 
--- src/AddToBalanceButton.jsx
+++ src/AddToBalanceButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AddToBalanceButton = (props) => {
   return (
-    <button type="button" onClick={() => props.addToBalance(props.addedVal)}>
+    <button type="button" className="add-button" onClick={() => props.addToBalance(props.addedVal)}>
       Add ${props.addedVal}
     </button>
   );
